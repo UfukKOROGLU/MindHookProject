3e9cdf5a9545ed2d35ac2fb02b38d6806dac997c
UfukKOROGLU <ukk102938@gmail.com>
2025-05-18 23:39:35 +0300

    Add Android client side of MindHookProject

--- app/src/main/java/com/example/project/ApiClient.kt
+++ app/src/main/java/com/example/project/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.0.2.2:5000/"
+    private const val BASE_URL = "http://YOUR_SERVER_IP/"
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
